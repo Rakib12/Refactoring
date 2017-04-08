aafda0af226cc25607f2065a853ed04df1bbe48a
Rakib12 <iamrakib1994@gmail.com>
2017-04-08 06:59:55 -0700

    after extracting calculate outstanding

--- Refactorable.java
+++ Refactorable.java
@@ -12,13 +12,18 @@ public class Refactorable {
 		printBanner();
 		
 		// calculate outstanding
+		outstanding = calculateOutstanding(e, outstanding);
+		
+		// print details
+		printDetail(outstanding);
+	}
+
+	public double calculateOutstanding(Enumeration<E> e, double outstanding) {
 		while(e.hasMoreElements()){
 			Order each = (Order) e.nextElement();
 			outstanding += each.getAmount();
 		}
-		
-		// print details
-		printDetail(outstanding);
+		return outstanding;
 	}
 
 	public void printDetail(double outstanding) {
@@ -33,4 +38,3 @@ public class Refactorable {
 	}
 
 }
-
